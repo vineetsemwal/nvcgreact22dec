4db73226373cbbe12f43400ef1f7261b87a800c8
vineetsemwal <vineetsemwal82@gmail.com>
2023-01-04 20:26:13 +0530

    string assignment

---
+++ StringUtil.java
@@ -0,0 +1,20 @@
+class StringUtil{
+
+
+  public static void main(Sring args[]){
+  
+  StringUtil demo=new StringUtil();
+  String result=demo.concatFeature("hello","world");
+  System.out.println(result);
+  
+  }
+
+
+ String concatFeature(String inp1, String inp2){
+   String result=inp1+inp2;
+   return result; 
+ }
+ 
+ 
+
+}
